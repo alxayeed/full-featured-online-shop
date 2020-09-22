939d485483717cdbef94e2561769c0698fad4780
al <alxayeed@gmail.com>
2020-09-22 13:15:57 +0600

    models added in shop app

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+# models for product catagory
+class Catagory(models.Model):
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'catagory'
+        verbose_name_plural = 'catagories'
+        ordering = ('name',)
+
+
+# model for Product
+class Product(models.Model):
+    catagory = models.ForeignKey(Catagory,
+                                 related_name="products",
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField(default=True)
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'))
+
+    def _str__(self):
+        return self.name
