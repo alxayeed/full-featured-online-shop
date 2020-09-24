4f91c7fb542a365a54f4a495140ab32ad8ad3f4e
al <alxayeed@gmail.com>
2020-09-24 13:13:28 +0600

    Cart class is created with all necessary methods

--- myshop/settings.py
+++ myshop/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',  # have a look on appconfig
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
