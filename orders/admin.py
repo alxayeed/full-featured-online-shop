a9cb59a52787d19fc5942c29ef0d4d4bad58c655
al <alxayeed@gmail.com>
2020-09-26 17:43:55 +0600

    admin template customized to show order details

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,5 @@
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 from django.contrib import admin
 from .models import Order, OrderItem
 
@@ -42,10 +44,15 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemAdmin]
     actions = [export_to_csv]
