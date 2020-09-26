5c287b371d98493f7796566b79ab1b00eda3f237
al <alxayeed@gmail.com>
2020-09-26 20:41:32 +0600

    order export as pdf feature added

--- orders/admin.py
+++ orders/admin.py
@@ -45,14 +45,25 @@ export_to_csv.short_description = 'Export to CSV'
 
 
 def order_detail(obj):
+    '''
+    Function to display order details in a customized admin  template
+    '''
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
+
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+
+order_pdf.short_description = 'Invoice'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemAdmin]
     actions = [export_to_csv]
--- orders/views.py
+++ orders/views.py
@@ -53,10 +53,18 @@ def admin_order_pdf(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     html = render_to_string('orders/order/pdf.html',
                             {'order': order})
+    # response = HttpResponse(content_type='application/pdf')
     response = HttpResponse(content_type='application/pdf')
+
+    # response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+
+    # weasyprint.HTML(string=html).write_pdf(response,
+    #                                        stylesheets=[weasyprint.CSS(
+    #                                            settings.STATIC_ROOT + 'css/pdf.css'
+    #                                        )])
     weasyprint.HTML(string=html).write_pdf(response,
-                                           stylesheet=[weasyprint.CSS(
-                                               settings.STATIC_ROOT + 'css/pdf.css'
-                                           )])
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT + 'css/pdf.css')])
+    print(type(response))
     return response
